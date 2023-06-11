84ffda1c0aaf882001d295787d45e2dfb088284b
Ayesha <ayesha18mbstu@gmail.com>
2023-06-11 21:50:59 +0600

    hide/unhide the password

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,67 +1,107 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
-import { FaGoogle } from 'react-icons/fa';
+import { FaGoogle, FaEye, FaEyeSlash } from "react-icons/fa";
+import "./Login.css";
 
 const Login = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const {logIn, googleLogIn} = useContext(AuthContext);
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
-    const onSubmit = data => {
-        console.log(data);
-        logIn(data.email, data.password)
-        .then(result => {
-          const user = result.user;
-          console.log(user);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { logIn, googleLogIn } = useContext(AuthContext);
 
-    const handleGoogleLogIn = () => {
-      googleLogIn()
-      .then(result => {
-          console.log(result.user);
+  const onSubmit = (data) => {
+    console.log(data);
+    logIn(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
       })
-      .catch(error =>  console.log(error));
-    };
-  
-    return (
-        <div className="hero min-h-screen bg-base-200 py-20">
-        <div className="hero-content flex-col">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login</h1>
-          </div>
-          <div className="card max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const handleGoogleLogIn = () => {
+    googleLogIn()
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200 py-20">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login</h1>
+        </div>
+        <div className="card max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
-                {errors.email && <span className="text-red-700 pt-2">Email is required</span>}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" {...register("password")} name="password" placeholder="password" className="input input-bordered" />
-              </div>
-              <div className="form-control mt-6">
-                <input className="btn btn-primary" type="submit" value="Login" />
-              </div>
-            </form>
-            <div><p className="px-8">New to this website? Please <Link className="font-bold text-blue-950" to="/register">Register</Link></p></div>
-            <div className="divider">OR</div>
-            <div className="text-center">
-            <button onClick={handleGoogleLogIn} className="btn btn-outline my-4"><FaGoogle className="mr-2" /> Login with Google</button>
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                {...register("email", { required: true })}
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+              {errors.email && (
+                <span className="text-red-700 pt-2">Email is required</span>
+              )}
             </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type={passwordVisible ? "text" : "password"}
+                {...register("password")}
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <span className="password-toggle-icon" onClick={togglePasswordVisibility}>
+                {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Login" />
+            </div>
+          </form>
+          <div>
+            <p className="px-8">
+              New to this website? Please
+              <Link className="font-bold text-blue-950" to="/register">
+                Register
+              </Link>
+            </p>
+          </div>
+          <div className="divider">OR</div>
+          <div className="text-center">
+            <button
+              onClick={handleGoogleLogIn}
+              className="btn btn-outline my-4"
+            >
+              <FaGoogle className="mr-2" /> Login with Google
+            </button>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
