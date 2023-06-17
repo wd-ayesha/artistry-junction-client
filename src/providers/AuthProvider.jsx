51a5d0abef03b7636137bafb5a2df18d1605e317
Ayesha <ayesha18mbstu@gmail.com>
2023-06-17 19:21:49 +0600

    jwt & axios added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -2,6 +2,7 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from './../firebase/firebase.config';
 import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 export const AuthContext =createContext(null);
 const auth = getAuth(app);
@@ -43,6 +44,18 @@ const logOut = () => {
             setUser(currentUser);
             console.log('current user', currentUser);
             setLoading(false);
+               // get and set token
+               if(currentUser){
+                axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+                .then(data =>{
+                    // console.log(data.data.token)
+                    localStorage.setItem('access-token', data.data.token)
+                    setLoading(false);
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
         });
         return () => {
             return unsubscribe();
